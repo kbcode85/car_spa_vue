76a057264c89c55b804a91a10862ee85ee12c48a
Kacper <90962091+KacperLes00@users.noreply.github.com>
2023-01-13 18:58:38 +0000

    homepage edit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="container flex flex-wrap justify-center mx-auto px-2 py-2.5">
-    hhfhd
-  </div>
+  <div class="flex p-4 justify-center">
+      
+      <NewCarCounter />
+  
+    </div>
 </template>
 
 <script>
+import NewCarCounter from '@/components/NewCarCounter.vue';
+export default{
+  components: {
+    NewCarCounter
+}
+}
 
 </script>
 
